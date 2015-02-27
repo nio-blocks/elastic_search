ae64fd5a8ab3f4b2716b212b19506671f7f2171b
Franky Martin <fmartin@neutral.io>
2015-02-27 14:11:17 -0500

    type (reserved word) property renamed to doc_type

--- es_base_block.py
+++ es_base_block.py
@@ -1,12 +1,15 @@
 from nio.common.block.base import Block
 from nio.common.command.params.dict import DictParameter
+from nio.common.command.params.string import StringParameter
 from nio.metadata.properties.expression import ExpressionProperty
 from nio.metadata.properties.string import StringProperty
 from nio.common.command import command
 from nio.common.discovery import Discoverable, DiscoverableType
 
 
+@command("search", DictParameter("params", default={}))
 @command("search", DictParameter("body", default={}))
+@command("search", StringParameter("doc_type", default=""))
 @command("connected")
 @Discoverable(DiscoverableType.block)
 class ESBase(Block):
@@ -19,7 +22,8 @@ class ESBase(Block):
 
     """
     index = StringProperty(title='Index', default="nio")
-    type = ExpressionProperty(title='Type', default="signals")
+    doc_type = ExpressionProperty(title='Type',
+                                  default="{{($__class__.__name__)}}")
 
     def __init__(self):
         super().__init__()
@@ -40,19 +44,20 @@ class ESBase(Block):
                                    format(s.to_dict(), str(e)))
 
     def _insert_signal(self, signal):
-
         body = signal.to_dict()
-        _type = self.type(signal)
+        doc_type = self.doc_type(signal)
 
         self._logger.debug("Inserting {} to: {}, type: {}".
-                           format(body, self.index, _type))
+                           format(body, self.index, doc_type))
 
-        self._es.index(index=self.index, doc_type=_type,
-                       body=body)
+        self._es.index(self.index, doc_type, body)
 
     def connected(self):
         return self._es.ping()
 
-    def search(self, body=None):
+    def search(self, doc_type, body=None, params=None):
+        doc_type = doc_type if doc_type else self.doc_type
         body = body if body else {}
-        return self._es.search(body=body, index=self.index)
+        params = params if params else {}
+        return self._es.search(index=self.index, doc_type=doc_type,
+                               body=body, params=params)
